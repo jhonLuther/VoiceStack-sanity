efa8974e36aa4baa78defcf22b4c3e94e3d197d7
hemanthv@carestack.com <hemanthv@carestack.com>
2024-12-11 12:38:54 +0530

    Feature Section Updates

--- src/components/features/FeatureSection.tsx
+++ src/components/features/FeatureSection.tsx
@@ -8,9 +8,11 @@ import TickIcon from './micro/icons/TickIcon'
 import PillItem from './micro/PillItem'
 import Button from '../common/Button'
 import ButtonArrow from '../icons/ButtonArrow'
+import PreText from './micro/PreText'
+import PhoneIcon from './micro/icons/PhoneIcon'
 
 
-const items = [
+const enterpriseItems = [
     "IVR & Call Routing",
     "Visual Voicemail",
     "Missed Call Response Tracking",
@@ -26,13 +28,14 @@ export default function FeatureSection() {
                 <div className='flex flex-col w-1/2 gap-8'>
 
                     <div className='flex flex-col gap-4'>
+                        <PreText><span className=' text-vs-blue'><PhoneIcon></PhoneIcon></span> Enterprise VoIP</PreText>
                         <H2>Comprehensive VoIP solutions for your practice</H2>
                         <Paragraph>Enhance patient experience with AI call scoring, analytics, and automation to improve communication and processes.</Paragraph>
                     </div>
                     <ul className='flex flex-wrap gap-3'>
                         {
 
-                            items.map((item, i) => {
+                            enterpriseItems.map((item, i) => {
                                 return (
                                     <PillItem key={i}> <span className='text-green-500'><TickIcon></TickIcon></span> {item}</PillItem>
                                 )
---
+++ src/components/features/micro/PreText.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function PreText({children}) {
+  return (
+    <div className='flex items-center gap-2 text-base font-medium'>{children}</div>
+  )
+}
