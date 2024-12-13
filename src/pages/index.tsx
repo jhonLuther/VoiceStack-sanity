e35ce3a1aa37efbebe8b9cff4aaef9027df0360a
shakir <shakirm@carestack.com>
2024-12-13 16:02:26 +0530

    file rename to avoid cache

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,6 +24,7 @@ import AnimatedBeamSection from '~/components/ui/animated/AnimatedBeamSection'
 import ComparisonSection from '~/components/ComparisonSection'
 import BannerSection from '~/components/BannerSection'
 import SiteComparisonSection from '~/components/SiteComparisonSection'
+import LinkCards from '~/components/LinkCardsSection'
 
 export const getStaticProps: GetStaticProps<SharedPageProps> = async ({
   draftMode = false,
