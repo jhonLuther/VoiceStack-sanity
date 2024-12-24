461599a93dd2a4e9e687352c869ade5be4764e02
Aravind_Cp <aravindcp@carestack.com>
2024-12-24 16:21:22 +0530

    sitemap.xml file changes

--- src/pages/sitemap.ts
+++ src/pages/sitemap.ts
@@ -1,20 +1,12 @@
 import fs from 'fs'
 import path from 'path'
 
-const SITE_URL = process.env.NEXT_PUBLIC_BASE_URL
+const SITE_URL = process.env.SANITY_SITE_URL
 
 export async function getStaticProps() {
-  const staticPaths = [
-    '',
-    '#testimonials-section',
-    '#about-us-section',
-    '#benefits-section',
-    '#integrations-section',
-    '#features-section',
-  ]
+
 
   const url = SITE_URL
-  const allPaths = [...staticPaths]
   const sitemapEntries = `<?xml version="1.0" encoding="UTF-8"?> 
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" 
             xmlns:xhtml="http://www.w3.org/1999/xhtml">
