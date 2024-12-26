79af9bea4afde42c5c0e5893b7861a92c40d1386
Akash <akashas@carestack.com>
2024-12-26 19:37:48 +0530

    hover removed

--- src/components/features/micro/ListItem.tsx
+++ src/components/features/micro/ListItem.tsx
@@ -16,7 +16,7 @@ export default function ListItem(props) {
         <div ref={liRef} style={{height: isMobile ? '100%' : `${actualPercentScrolled}%`}} className={`w-1 ${props.showDesc == true ? "bg-vs-blue" : "bg-transparent"} max-h-[100%] h-full rounded-full`}></div>
       </div>
       <div className={`flex flex-col ease-linear px-2 transition-all duration-300 ${props.showDesc? 'py-3':' '} `}>
-        <H3 className={`group-hover:text-vs-blue ${props.showDesc? '!text-lg':' opacity-50'} transition-all duration-300`}>{props.title}</H3>
+        <H3 className={` ${props.showDesc? '!text-lg':' opacity-50'} transition-all duration-300`}>{props.title}</H3>
         <div className={`overflow-y-hidden ease-linear ${props.showDesc ? 'opacity-100':' h-0 opacity-1'}`}>
           <Paragraph className={`flex `}>{props.desc}</Paragraph>
         </div>
