818a832cf5d6c6daacf51cc53e1ae15e511f2121
Shakir <shakirm@carestack.com>
2024-12-27 13:51:19 +0000

    Merged in dev (pull request #40)
    Dev
    
    * lpform added
    
    * sitemap xml file change
    
    * sitemap xml file change
    
    * Merged in sitemapFilestatic (pull request #33)
    
    SitemapFilestatic
    
    * Safari Fix - Scroll Flickering
    
    * Merge branch 'dev' of https://bitbucket.org/cstack-bb/voicestack-sanity into dev
    
    * intermediate icon changed
    
    * Merge branch 'dev' of https://bitbucket.org/cstack-bb/voicestack-sanity into dev
    
    * Merge branch 'main' of https://bitbucket.org/cstack-bb/voicestack-sanity into dev
    
    * Feature Fixes
    
    * Merge branch 'dev' of https://bitbucket.org/cstack-bb/voicestack-sanity into dev
    
    * merge fix
    
    * build fix
    
    * merge fix
    
    * Feature Fixes - Mobile
    
    * Merge branch 'main' of https://bitbucket.org/cstack-bb/voicestack-sanity into dev
    
    * Updated GTag Events
    
    * Merge branch 'main' of https://bitbucket.org/cstack-bb/voicestack-sanity into dev
    
    * console removed
    
    * Merge branch 'dev' of https://bitbucket.org/cstack-bb/voicestack-sanity into dev
    
    * Merge branch 'main' of https://bitbucket.org/cstack-bb/voicestack-sanity into dev
    
    * title change
    
    * gtm moved to app.tsx
    
    * Merge branch 'main' of https://bitbucket.org/cstack-bb/voicestack-sanity into dev
    
    * build fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import type { AppProps } from 'next/app'
 import { IBM_Plex_Mono, Inter, PT_Serif,Poppins } from 'next/font/google'
 import { lazy } from 'react'
 import Layout from '../components/Layout'
+import Script from 'next/script'
 
 export interface SharedPageProps {
   heroSectionData(heroSectionData: any): any
@@ -50,6 +51,41 @@ export default function App({
   const { draftMode, token } = pageProps
   return (
     <>
+    {/* Google Analytics */}
+    <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-YY0CHYH7EY"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-YY0CHYH7EY');
+          `}
+        </Script>
+
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-KCX7H59S');
+          `}
+        </Script>
+
+        {/* Start cookieyes banner */}
+        <Script id="cookieyes" strategy="afterInteractive" 
+          src="https://cdn-cookieyes.com/client_data/892b60d226bd40003a3303d6/script.js">
+        </Script>
+
+        {/* <!--[BEGIN Google Tag Manager (noscript)]--> */}
+	      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KCX7H59S" height="0" width="0"
+			    style={{ display: 'none', visibility: 'hidden' }}></iframe></noscript>
+	      {/* <!--[END Google Tag Manager (noscript)]--> */}
+    
       <style jsx global>
         {`
           :root {
