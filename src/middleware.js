fc22a8b2f9f99e08d4be77d088098fe52177c174
shakir <shakirm@carestack.com>
2024-12-30 13:44:26 +0530

    for build

--- src/middleware.js
+++ src/middleware.js
@@ -3,6 +3,18 @@ import { NextResponse } from 'next/server'
 export async function middleware(request) {
   const region = request.nextUrl.href
   const response = NextResponse.next()
+  const { geo } = request;
+  const country = geo?.country || 'US'
+  const city = geo.city || 'San Francisco'
+  const userRregion = geo.region || 'CA'
+
+  const countryVersion = (country === "UM" || country === "US") ? 1 : (country === "UK" || country === "GB") ? 2 : (country === "AU") ? 3 : 4;
+
+  const countryLocale = (countryVersion === 1) ? "en" : (countryVersion === 2) ? "en-GB" : (countryVersion === 3) ? "en-AU" : undefined;
+
+  console.log({countryVersion}, {countryLocale});
+  
+  
 
   // Determine the cookie based on the region
   if (region.includes('en-GB')) {
