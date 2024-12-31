243f65b27c1cadaf1b4d82d69e8b69bde4327500
shakir <shakirm@carestack.com>
2024-12-31 14:41:53 +0530

    popup updates

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -14,6 +14,7 @@ import ChevronUp from '../icons/ChevronDown';
 import Script from 'next/script';
 import useMediaQuery from '~/utils/mediaQuery';
 import { BookDemoContext } from '~/providers/BookDemoProvider';
+import { getCookie, setCookie } from '~/utils/cookie';
 
 
 
@@ -26,6 +27,22 @@ const Header = ({ data }) => {
   const [openSwitcher, setOpenSwitcher] = useState(false);
   const [currentRegion, setCurrentRegion] = useState(null);
   const { isDemoPopUpShown } = useContext(BookDemoContext);
+  const [countryCode, setCountryCode] = useState<any>();
+  const [regionSwitcher, setRegionSwitcher] = useState(false);
+  const [regionSwitcherTop, setRegionSwitcherTop] = useState(false);
+  const [currentCountry, setCurrentCountry] = useState<any>(null);
+  
+  const geoPath ="/api/geo";
+  const preLocale = getCookie("__vs_pl");
+
+  const getGeoData = async () => {
+    try {
+      return (await (await fetch(geoPath)).json())
+    } catch (error) {
+      console.log(error);
+      
+    }
+  }
 
   const regions = [
     {
@@ -35,7 +52,7 @@ const Header = ({ data }) => {
       },
       "url": "/",
       "title": "US",
-      "locale": "en"
+      "Country": "en"
     },
     {
       "flag": {
@@ -62,6 +79,89 @@ const Header = ({ data }) => {
   const matchedRegion = regions.find((region) => region.locale === router.locale);
   const toggleRef = useRef(null);
   const isMobile = useMediaQuery(767);
+
+  console.log({matchedRegion});
+  
+
+
+  // useEffect(() => {
+
+  //   //custom geo api if middleware failed
+  //   if (window !== undefined) {
+  //     const country:any = getCookie("__vs_ver");
+  //     const hasCountrySet = country !== null;
+
+  //     if (!hasCountrySet) {
+  //       console.log("Not getting cc from mw");
+        
+  //       const handleGeoData = async () => {
+  //         try {
+  //           const res = await getGeoData();
+  //           if (!res || !res.country) {
+  //             console.log("No country data available.");
+  //             setCookie("__vs_ver", "1");
+  //             setCookie("__vs_pl", "en");
+  //             return;
+  //           }
+        
+  //           // Determine country code based on the country
+  //           const countryCodeNum:any =
+  //           res.country === "UM" || res.country === "US" ? 1 : res.country === "UK" || res.country === "GB" ? 2 : res.country === "AU"  || res.country === "NZ" ? 3 : 4;
+                
+  //           // Set the country code state and cookie
+  //           setCountryCode(countryCodeNum);
+  //           setCookie("__vs_ver", countryCodeNum);
+        
+  //           // Set the locale cookie based on the country
+  //           // switch (res.country) {
+  //           //   case "GB":
+  //           //   case "UK":
+  //           //     setCookie("__vs_pl", "en-GB");
+  //           //     break;
+  //           //   case "AU":
+  //           //   case "NZ":
+  //           //     setCookie("__vs_pl", "en-AU");
+  //           //     break;
+  //           //   case "UM":
+  //           //   case "US":
+  //           //     setCookie("__vs_pl", "en");
+  //           //     break;
+  //           //   default:
+  //           //     setCookie("__vs_pl", "en");
+  //           // }
+  //         } catch (error) {
+  //           console.error("Error fetching geo data:", error);
+  //         }
+  //       };
+        
+  //       // Call the function
+  //       handleGeoData();
+  //     } 
+  //     else {
+  //       setCountryCode(country);
+  //     }
+  //   }
+
+  //   //for showing region main popup
+  //   function shouldRenderPopup() {
+  //     const countryCd:any = getCookie("__cs_ver") ? getCookie("__cs_ver") : 1;
+  //     return (
+  //       preLocale == null &&
+  //       countryCd != "undefined" 
+  //     );
+  //   }
+  //   setTimeout(() => {
+  //     setRegionSwitcher(shouldRenderPopup);
+  //   }, 500);
+
+  //   //for currentlocale
+  //   setCurrentCountry(
+  //     router.locale == "en" ? "USA" : 
+  //     router.locale == "en-GB" ? "UK" : 
+  //     router.locale == "en-AU" ? "ANZ" : undefined
+  //   );
+
+  // }, [router])
   
 
   useEffect(()=>{
@@ -91,6 +191,7 @@ const Header = ({ data }) => {
   const toggleSwitcher = () => {
     setOpenSwitcher(!openSwitcher)
   }
+
   useEffect(() => {
     const handleBodyClick = (event) => {
       if (toggleRef.current && !toggleRef.current.contains(event.target)) {
@@ -118,9 +219,41 @@ const Header = ({ data }) => {
   const before = "before:content-[''] before:h-[100px] before:absolute before:left-0 before:right-0 before:top-full before:bg-zinc-900";
   return (
     <>
-        
+    {/* region popup main */}
+    {/* {regionSwitcher &&  
+      <div className="fixed bg-[hsla(0,0%,9%,0.6)] h-screen w-screen z-[999] flex justify-center items-center">
+        <div className="bg-white mx-auto pt-10 p-6 rounded-lg flex items-center flex-col gap-6 relative w-[310px]">
+          <svg onClick={() => {
+            setRegionSwitcher(false);
+            setCookie('__vs_pl', router.locale ?? "en")  
+          }} className='absolute right-3 top-3 cursor-pointer' 
+            xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+            <path fillRule="evenodd" clipRule="evenodd" d="M4.29279 4.29357C4.48031 4.1061 4.73462 4.00078 4.99979 4.00078C5.26495 4.00078 5.51926 4.1061 5.70679 4.29357L9.99979 8.58657L14.2928 4.29357C14.385 4.19806 14.4954 4.12188 14.6174 4.06947C14.7394 4.01706 14.8706 3.98947 15.0034 3.98832C15.1362 3.98717 15.2678 4.01247 15.3907 4.06275C15.5136 4.11303 15.6253 4.18728 15.7192 4.28117C15.8131 4.37507 15.8873 4.48672 15.9376 4.60962C15.9879 4.73251 16.0132 4.86419 16.012 4.99697C16.0109 5.12975 15.9833 5.26097 15.9309 5.38297C15.8785 5.50498 15.8023 5.61532 15.7068 5.70757L11.4138 10.0006L15.7068 14.2936C15.8889 14.4822 15.9897 14.7348 15.9875 14.997C15.9852 15.2592 15.88 15.51 15.6946 15.6954C15.5092 15.8808 15.2584 15.986 14.9962 15.9882C14.734 15.9905 14.4814 15.8897 14.2928 15.7076L9.99979 11.4146L5.70679 15.7076C5.51818 15.8897 5.26558 15.9905 5.00339 15.9882C4.74119 15.986 4.49038 15.8808 4.30497 15.6954C4.11956 15.51 4.01439 15.2592 4.01211 14.997C4.00983 14.7348 4.11063 14.4822 4.29279 14.2936L8.58579 10.0006L4.29279 5.70757C4.10532 5.52004 4 5.26573 4 5.00057C4 4.73541 4.10532 4.4811 4.29279 4.29357Z" fill="black" />
+          </svg>
 
-      
+          <p className='text-center text-gray-800 font-medium text-base leading-[1.5]'>You are currently viewing VoiceStack&apos;s website for the<br/> {currentCountry} region</p>
+
+          <Button type="primary" onClick={() => {
+            setRegionSwitcher(false);
+            setCookie('__vs_pl', router.locale ?? "en")}}
+          >
+            <span className="text-base font-medium">
+              Continue with VoiceStack {currentCountry}
+            </span>
+          </Button>
+          <div className='w-full'>
+            <div className="w-full flex justify-center relative after:content-[''] after:absolute after:left-0 after:top-1/2 after:border-b after:border-[#E5E7EB] after:right-0 after:-z-1">
+              <span className='flex px-3 bg-white relative z-1 text-gray-400'>Or Go to</span>
+            </div>
+            <div className='flex items-center justify-between gap-5'>
+            
+            </div>
+          </div>
+        </div>
+      </div>
+    } */}
+      {/* region popup main end*/}
+
       <div className={`relative w-full before:content-[''] before:-z-0 before:h-[100px] before:absolute before:left-0 before:right-0 before:top-[-100px] before:bg-vs-blue`}>
         <header
           className={`fixed w-full top-0 lg:top-[35px] left-0 z-20 transition-all duration-300 ease-linear ${headerFixed && '!fixed w-full lg:!top-4'}  left-0`}      >
@@ -312,7 +445,7 @@ const Header = ({ data }) => {
                   </div>
                 </div>
               )}
-              {/* Region Switcher Desktop ./ */}
+              {/* Region Switcher Desktop end */}
             </div>
           </div>
         </header>
