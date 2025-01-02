6d5a2214aa3f6b6da066246389dd59703ef5c5f9
Shakir <shakirm@carestack.com>
2025-01-02 16:11:58 +0000

    Merged in dev (pull request #58)
    Dev
    
    * Merge branch 'main' of https://bitbucket.org/cstack-bb/voicestack-sanity into dev
    
    * Merge branch 'dev' of https://bitbucket.org/cstack-bb/voicestack-sanity into geo
    
    * title and alt fixes
    
    * Merge branch 'dev' of https://bitbucket.org/cstack-bb/voicestack-sanity into featurebugfix
    
    * Merged in featurebugfix (pull request #54)
    
    Hot Quick Fixes =>  Removed unused code and fixed alt title not showing issue in feature images
    
    Approved-by: Shakir
    
    * top banner region
    
    * header region banner
    
    * build fix
    
    * build fix
    
    * dependency change
    
    * Merge branch 'dev' of https://bitbucket.org/cstack-bb/voicestack-sanity into geo
    
    * empty dependency for top region banner
    
    * build check
    
    * build fix
    
    * build fix 1
    
    * build fix2
    
    * top region banner fix
    
    * consoles removed
    
    * geo merged
    
    * Merge branch 'main' of https://bitbucket.org/cstack-bb/voicestack-sanity into dev
    
    * padding change for top region banner
    
    * removed encodeurlcomponent
    
    * Merge branch 'main' of https://bitbucket.org/cstack-bb/voicestack-sanity into dev
    
    * Merge branch 'main' of https://bitbucket.org/cstack-bb/voicestack-sanity into dev
    
    * facebook pixel added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -85,6 +85,36 @@ export default function App({
           </Script>
         )}
 
+      {/* Meta Pixel Code */}
+      <Script
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '474197578586490');
+            fbq('track', 'PageView');
+          `,
+        }}
+      />
+
+      {/* Fallback for users with disabled JavaScript */}
+      <noscript>
+        <img
+          height="1"
+          width="1"
+          style={{ display: 'none' }}
+          src="https://www.facebook.com/tr?id=474197578586490&ev=PageView&noscript=1"
+          alt="facebook pixel"
+        />
+      </noscript>
+
         {/* <!--[BEGIN Google Tag Manager (noscript)]--> */}
 	      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KCX7H59S" height="0" width="0"
 			    style={{ display: 'none', visibility: 'hidden' }}></iframe></noscript>
