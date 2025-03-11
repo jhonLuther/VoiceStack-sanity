670c7a08406781eb9519eb72bf663d8946336c4d
Akash <akashas@carestack.com>
2025-03-11 18:58:35 +0530

    feature fixes

--- src/pages/features/[slug].tsx
+++ src/pages/features/[slug].tsx
@@ -5,12 +5,15 @@ import { getClient } from '~/lib/sanity.client'
 import { readToken } from '~/lib/sanity.api'
 import { SanityClient } from 'sanity'
 import Header from '~/components/common/Header'
-import { getALLHomeSettings, getFeaturePageData } from '~/lib/sanity.queries'
+import { fetchFaq, getALLHomeSettings, getBannerData, getFeaturePageData, getFooterData } from '~/lib/sanity.queries'
 import { getHeroSectionData } from '~/lib/sanity.queries'
-import { useContext, useEffect } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import { BookDemoContext } from '~/providers/BookDemoProvider'
 import FeatureBenefitSection from '~/components/features/FeatureBenefitSection'
 import FeatureImageSection from '~/components/features/FeatureImageSection'
+import FaqSection from '~/components/FaqSection'
+import BannerSection from '~/components/BannerSection'
+import Footer from '~/components/common/Footer'
 
 interface PageProps {
   page: {
@@ -20,27 +23,34 @@ interface PageProps {
     content: any[]
     // Add other fields you need
   }
-  homeSettings:any;
-  heroData:any;
-  region: string
+  homeSettings: any;
+  heroData: any;
+  region: string;
+  faqSectionData: any;
+  bannerData: any;
+  footerData: any;
 }
 
-export default function Page({ page, homeSettings, heroData, region }: PageProps) {
+export default function Page({ page, homeSettings, heroData, region, faqSectionData, bannerData, footerData }: PageProps) {
   const { isDemoPopUpShown, setIsDemoPopUpShown } = useContext(BookDemoContext);
-  
+  const [refer, setRefer] = useState(null);
+
   useEffect(() => {
     setIsDemoPopUpShown(heroData);
-  },[heroData])
+  }, [heroData])
 
-  console.log(page,'page');
-  
-  
+  console.log(page, 'page');
+
+
   if (!page) return <div>Page not found</div>
   return (
     <>
       <Header data={homeSettings} />
-        <FeatureBenefitSection data={page?.featureSubSection}/>
-        <FeatureImageSection data={page?.featureBenefitsSection}/>
+      <FeatureBenefitSection data={page?.featureSubSection} />
+      <FeatureImageSection data={page?.featureBenefitsSection} />
+      <FaqSection data={page.featureFAQSection} mailId={''} />
+      <BannerSection data={bannerData} refer={refer}></BannerSection>
+      <Footer data={footerData}></Footer>
     </>
   )
 }
@@ -56,14 +66,17 @@ export const getStaticProps: GetStaticProps<PageProps> = async ({
 }) => {
   const region = locale
   const slug = params?.slug as string
-  
-  const client = getClient(draftMode ? { token: readToken } : undefined) as SanityClient
-  const [page, homeSettings, heroData] = await Promise.all([
-    getFeaturePageData(client, slug, region),
-    getALLHomeSettings(client, region),
-    getHeroSectionData(client, region),
 
-  ])
+  const client = getClient(draftMode ? { token: readToken } : undefined) as SanityClient
+  const [page, homeSettings, heroData, faqSectionData, footerData, bannerData] = await Promise.all([
+      getFeaturePageData(client, slug, region),
+      getALLHomeSettings(client, region),
+      getHeroSectionData(client, region),
+      fetchFaq(client, region),
+      getFooterData(client, region),
+      getBannerData(client, region)
+
+    ])
 
   if (!page) {
     return {
@@ -76,7 +89,8 @@ export const getStaticProps: GetStaticProps<PageProps> = async ({
       page,
       homeSettings,
       heroData,
-      region
+      region,
+      faqSectionData, footerData, bannerData
     },
     // revalidate: 60
   }
@@ -84,22 +98,22 @@ export const getStaticProps: GetStaticProps<PageProps> = async ({
 
 export const getStaticPaths: GetStaticPaths = async () => {
   const client = getClient()
-  
+
   // Get all pages with their slugs and languages
   const query = groq`*[_type == "featureList"]{
     "slug": slug.current,
     language
   }`
-  
+
   const pages = await client.fetch(query)
-  
+
   // Create paths for each page in each language
   const paths = pages.map((page: any) => ({
-    params: { 
+    params: {
       slug: page.slug
     },
     // This matches your existing locale pattern
-    locale: page.language 
+    locale: page.language
   }))
 
   return {
