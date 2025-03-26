e52a1934d97a0da65fa37df24e8b7fe55df91aca
Akash <akashaanil32@gmail.com>
2025-03-26 12:52:58 +0530

    fixes package reverted

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    experimental: {
-      esmExternals: false, // Forces Next.js to use CommonJS
-    },
-  };
-  
-  module.exports = nextConfig;
-  
